cf2e9a2377ee10c86cbd410ced877f6f312b1823
Eliseu Silva <eliseusilva.info@gmail.com>
2025-09-25 02:08:31 +0100

    feat: refactor Header component to remove unused elements and update navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,30 @@
-import { Link } from "react-router-dom";
 import "./Header.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <div className="logo">
-          <Link to="/">
-            <h2 className="text-glow terminal-text">[ KOLTE.DEV ]</h2>
-          </Link>
-        </div>
         <nav className="nav">
-          <ul>
-            <li>
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-            </li>
-            <li>
-              <span className="nav-link disabled" title="Coming Soon">
-                Projects
-              </span>
-            </li>
-            <li>
-              <span className="nav-link disabled" title="Coming Soon">
-                Blog
-              </span>
-            </li>
-            <li>
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
+          <a
+            href="https://linkedin.com/in/eliseu03"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-link"
+          >
+            <FaLinkedin className="icon" />
+            <span className="text">LinkedIn</span>
+          </a>
+
+          <a
+            href="https://github.com/eliseufh"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-link"
+          >
+            <FaGithub className="icon" />
+            <span className="text">GitHub</span>
+          </a>
         </nav>
       </div>
     </header>
